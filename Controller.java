959c0b34d8736a02c9456f26834a8f18b311a45c
alexmoisan <alex_moisan@hotmail.com>
2020-02-04 16:40:20 -0500

    Ajout Exercice 3

--- Controller.java
+++ Controller.java
@@ -1,11 +1,14 @@
 package sample;
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.*;
 
 public class Controller {
     public TextField txfSource;
+    public ComboBox cmbTarget;
 
+    /*
     public void txfSourceDragDetected(MouseEvent mouseEvent) {
         if (txfSource.getText().length() > 0){
             Dragboard db = txfSource.startDragAndDrop(TransferMode.ANY);
@@ -16,6 +19,7 @@ public class Controller {
 
     }
 
+    //Exercice 1
     public void imgDragOver(DragEvent dragEvent) {
         dragEvent.acceptTransferModes(TransferMode.ANY);
     }
@@ -23,5 +27,40 @@ public class Controller {
     public void imgDragDropped(DragEvent dragEvent) {
         Dragboard db = dragEvent.getDragboard();
         txfSource.clear();
+    }
+
+     */
+
+    //Exercice 2
+    public void cmbDragDropped(DragEvent dragEvent) {
+        Dragboard db = dragEvent.getDragboard();
+        cmbTarget.getItems().addAll(txfSource.getText());
+        cmbTarget.getSelectionModel().selectLast();
+        txfSource.clear();
+    }
+
+    public void cmbDragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    //Exercice 3
+    public void txfSourceDragDetected(MouseEvent mouseEvent) {
+        TextField ref = (TextField) mouseEvent.getSource();
+        if (ref.getText().length() > 0) {
+            Dragboard db = ref.startDragAndDrop((TransferMode.ANY));
+            ClipboardContent content = new ClipboardContent();
+            content.putString(ref.getText());
+            db.setContent(content);
+        }
+    }
+
+    public void imgDragDropped(DragEvent dragEvent) {
+        TextField refSource = (TextField) dragEvent.getGestureSource();
+        Dragboard db = dragEvent.getDragboard();
+        refSource.clear();
+    }
+
+    public void imgDragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
     }
 }
